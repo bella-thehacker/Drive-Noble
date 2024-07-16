f0753aa93ead0fd5108da49b7cb0a368b478c0da
Ivy Nyambura <ikagam018@gmail.com>
2024-07-16 22:36:35 +0300

    added footer

--- mercedes.js
+++ mercedes.js
@@ -1,3 +1,5 @@
+
+
 const url = "http://localhost:3000/mercedes";
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -9,6 +11,49 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 3000);
 });
 
-function mostPopular(){
-    const cars = document.querySelector(".cars")
-}
+// function fetchData() {
+//   fetch(url)
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw Error("error");
+//       }
+//       return res.json();
+//     })
+//     .then(mostPopular)
+//     .catch((error) => {
+//       console.error("error fetching data", error);
+//     });
+// }
+
+// function createAnElement(element) {
+//   return document.createElement(element);
+// }
+
+// function addText(element, text) {
+//   element.innerText = text;
+// }
+
+// function listen(element, event, callback) {
+//   return element.addEventListener(event, callback);
+// }
+
+// const mostPopular = (mercedes.data) => {
+//   const cars = document.querySelector(".cars");
+//   cars.innerHTML = ""
+//   const popular = createAnElement("div");
+//   popular.className = "popular";
+//   cars.appendChild(popular);
+  
+
+
+//   mercedes.data.map((mercedes) => {
+//     const carImg = createAnElement("img");
+//     carImg.className = "carImg";
+//     poster.src = mercedes.poster;
+//     popular.appendChild(carImg);
+  
+//   });
+
+  
+// }
+
