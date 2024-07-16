60bfb412f7dc60f52a0198e0cdbc72cf1177c99a
Ivy Nyambura <ikagam018@gmail.com>
2024-07-16 21:23:04 +0300

    removed fiter beacause it brought bugs

--- mercedes.js
+++ mercedes.js
@@ -1,32 +1,14 @@
-const url = "http://localhost:3000/mercedes"
+const url = "http://localhost:3000/mercedes";
 
-document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(function(){
-        document.querySelector(".wrapper").classList.add("display")
-        document.querySelector("nav").classList.add("later")
-        document.querySelector(".body").classList.add("later")
-        document.querySelector(".head").classList.add("show")
-    }, 3000)
-        
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.querySelector(".wrapper").classList.add("display");
+    document.querySelector("nav").classList.add("later");
+    document.querySelector(".body").classList.add("later");
+    document.querySelector(".head").classList.add("show");
+  }, 3000);
+});
 
 function mostPopular(){
-    fetch(url)
-    .then(res => res.json())
-    .then((data) =>{
-        const cars = document.querySelector(".cars")
-
-        cars.innerHTML = `
-        <img src="${mercedes.poster}" alt="${mercedes.name}"/>
-        <h3>Mercedes-Benz${mercedes.name}</h3>
-        <p> Price ${mercedes.price}</p>
-        <p> Horsepower${mercedes.horsepower}`
-        
-        
-
-       
-    })
+    const cars = document.querySelector(".cars")
 }
-
-
