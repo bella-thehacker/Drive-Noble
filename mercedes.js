0e1697900ac9f198f8b3c567311fed78ac1d7415
Ivy Nyambura <ikagam018@gmail.com>
2024-07-16 11:12:49 +0300

    made nav bar

---
+++ mercedes.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(function(){
+        document.querySelector(".wrapper").classList.add("display")
+        document.querySelector(".head").classList.add("show")
+    }, 3000)
+        
+    
+})
+
