dec067255798341f7d6cb9dd4616b9214ea4eabc
Ivy Nyambura <ikagam018@gmail.com>
2024-07-16 15:03:51 +0300

    input data into json file

--- mercedes.js
+++ mercedes.js
@@ -1,9 +1,22 @@
+const url = "http://localhost:3000/mercedes"
+
 document.addEventListener("DOMContentLoaded", function(){
     setTimeout(function(){
         document.querySelector(".wrapper").classList.add("display")
+        document.querySelector("nav").classList.add("later")
+        document.querySelector(".body").classList.add("later")
         document.querySelector(".head").classList.add("show")
     }, 3000)
         
     
 })
+
+function mostPopular(){
+    fetch(url)
+    .then(res => res.json())
+    .then((data) =>{
+        
+    })
+}
+
 
